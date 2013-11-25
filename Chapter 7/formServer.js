67c01c7d14dc827683621d8fca30812a54714e60
abraharl <lee_abbraham@yahoo.com>
2013-11-24 18:11:10 -0600

    added chapter 7

---
+++ Chapter 7/formServer.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+console.log('server started');
+
+http.createServer(function(req,res){
+	if(req.url == '/'){
+	  res.writeHead(200,{'Content-Type':'text/html'});
+	  res.end(['<form method="POST" action="/url">',
+		     '<h1>My Form</h1>',
+		       '<fieldset>',
+		       '<label>Personal Information</label>',
+		       '<p>What is your name?</p>',
+		       '<input type="text" name="name">',
+		       '<p><button>Submit</button></p>',
+		     '</form>'].join(''));
+    }
+    else if(req.url == '/url'){
+      res.writeHead(200,{'Content-Type':'text/html'});
+      res.end('You sent a <em>'+req.method+'</em> request');
+    }
+}).listen(3001);
